d8bffcad083f23839a7dcfd27608d21afedaff43
lyn <buaa_lyn@163.com>
2014-09-18 16:33:15 +0800

    把游记的详情弄进去了，但是相关的还没有，样式也得讨论一下

--- pi/routes/route.js
+++ pi/routes/route.js
@@ -6,12 +6,12 @@ var model = require('../model/sup_model.js');
 var apiList = require('../url_api');
 
 router.get('/detail/:ROUTEID', function(req, res) { 
-  model.setUrl(apiList.apiHost + apiList.routePageDetail);
-  model.getdata(req, function(data) {
-    console.log("复制路线的Url:" + model.getUrl());
-    data = JSON.parse(data);
-    res.render('route/detail', data);
-  })
-})  
+    model.setUrl(apiList.apiHost + apiList.routePageDetail);
+    model.getdata(req, function(data) {
+        console.log("复制路线的Url:" + model.getUrl());
+        data = JSON.parse(data);
+        res.render('route/detail', data.result);
+    })
+})
 
 module.exports = router;
