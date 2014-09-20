36a90603eba313b56c7565f031865d96eeb2a887
lyn <buaa_lyn@163.com>
2014-09-20 16:58:31 +0800

    登录和悬浮

--- pi/public/javascripts/lib/login.js
+++ pi/public/javascripts/lib/login.js
@@ -1,9 +1,9 @@
 $(function () {
-    var topHd=$('#top'),
-        lgLayer=$('.lg-layer');
-        layer=$('.layer');
-    topHd.on('click','a.btn-login',function(e){
+    var topHd = $('#top'),
+        lgLayer = $('.lg-layer');
+        layer = $('.layer');
         
+    topHd.on('click','a.login',function(e){
         lgLayer.show(500);
         layer.show();
         lgLayer.show(500,function(){ 
@@ -16,4 +16,13 @@ $(function () {
             })
         });
     })
+    
+    var weibo = $('#weibo_login'),
+        qq = $('#qq_login');
+    weibo.mousedown(function(e){
+        $(this).attr("src","/images/common/login/weibo_login_hover.png");
+    })
+    weibo.mousedown(function(e){
+        $(this).attr("src","/images/common/login/weibo_login_hover.png");
+    })   
 })
