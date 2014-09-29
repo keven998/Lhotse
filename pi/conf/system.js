a2bffb2ffc6166224474cf2216c4ccdd2fe0ae50
Demien Li <demien@appannie.com>
2014-09-29 19:10:35 +0800

    update product weibo qq key

--- pi/conf/system.js
+++ pi/conf/system.js
@@ -14,10 +14,10 @@ if (app.get('env') === 'development' || app.get('env') === 'local'){
     config['qq_client_secret'] = '124f82692bc1080c0af901da4c379ac5';
 }else if(app.get('env') === 'product'){
     config['domain'] = 'www.lvxingpai.com';
-    config['weibo_client_id'] = '';
-    config['weibo_client_secret'] = '';
-    config['qq_client_id'] = '';
-    config['qq_client_secret'] = '';
+    config['weibo_client_id'] = '2138367538';
+    config['weibo_client_secret'] = '8264a7394136219fe1c6394f362f4bc2';
+    config['qq_client_id'] = '101157594';
+    config['qq_client_secret'] = 'a7395f1441b40b09047cdca9c8b04d37';
 }
 
 config['weibo_call_back'] = 'https://api.weibo.com/oauth2/authorize?client_id=' + config.weibo_client_id + '&response_type=code&redirect_uri=http://' + config.domain + '/account/callback/weibo/'
