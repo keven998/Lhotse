d900f9215b85430054ba42347697e939ded0af14
Demien Li <demien@appannie.com>
2014-09-29 18:53:17 +0800

    add env & config

---
+++ pi/conf/system.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var config = {};
+
+if (app.get('env') === 'development' || app.get('env') === 'local'){
+    if (app.get('env') === 'development') {
+        config['domain'] = 'www2.lvxingpai.cn'; 
+    }else if(app.get('env') === 'local') {
+        config['domain'] = 'www2.lvxingpai.cn:8880';
+    }
+    config['weibo_client_id'] = '2294159543';
+    config['weibo_client_secret'] = 'a35ae59c1883bf184e7b76c667e88cee';
+    config['qq_client_id'] = '101151725';
+    config['qq_client_secret'] = '124f82692bc1080c0af901da4c379ac5';
+}else if(app.get('env') === 'product'){
+    config['domain'] = 'www.lvxingpai.com';
+    config['weibo_client_id'] = '';
+    config['weibo_client_secret'] = '';
+    config['qq_client_id'] = '';
+    config['qq_client_secret'] = '';
+}
+
+config['weibo_call_back'] = 'https://api.weibo.com/oauth2/authorize?client_id=' + config.weibo_client_id + '&response_type=code&redirect_uri=http://' + config.domain + '/account/callback/weibo/'
+config['qq_call_back'] = 'https://graph.qq.com/oauth2.0/authorize?client_id=' + config.qq_client_id + '&response_type=code&redirect_uri=http://' + config.domain + '/account/callback/qq/&state=01'
+
+
+module.exports = config;
