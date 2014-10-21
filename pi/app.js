8b382dc0ef618a3c7b284eed173342d2fd4ea725
Demien Li <demien@appannie.com>
2014-10-21 21:56:57 +0800

    close log

--- pi/app.js
+++ pi/app.js
@@ -17,43 +17,43 @@ var hotel = require('./routes/hotel');
 var app = express();
 
 // log4js settings
-log4js.configure({
-    appenders: [
-        {
-            type: 'console',
-            layout: {
-                type: "pattern",
-                pattern: "%d{MM/dd hh:mm} %-5p %m"
-            }
-        },
-        {
-            type: 'dateFile',
-            filename: path.join(__dirname, 'logs/blah.log'),
-            absolute: true,
-            pattern: "-yyyy-MM-dd",
-            alwaysIncludePattern: true,
-            category: 'normal',
-            layout: {
-                type: "pattern",
-                pattern: "%d{MM/dd hh:mm} %-5p %m"
-            }
-        }
-    ],
-    replaceConsole: true
-});
+// log4js.configure({
+//     appenders: [
+//         {
+//             type: 'console',
+//             layout: {
+//                 type: "pattern",
+//                 pattern: "%d{MM/dd hh:mm} %-5p %m"
+//             }
+//         },
+//         {
+//             type: 'dateFile',
+//             filename: path.join(__dirname, 'logs/blah.log'),
+//             absolute: true,
+//             pattern: "-yyyy-MM-dd",
+//             alwaysIncludePattern: true,
+//             category: 'normal',
+//             layout: {
+//                 type: "pattern",
+//                 pattern: "%d{MM/dd hh:mm} %-5p %m"
+//             }
+//         }
+//     ],
+//     replaceConsole: true
+// });
 
 // set logger_log4js can be called in other files
-exports.logger_log4js = function(name) {
-  var logger4js = log4js.getLogger(name);
-  logger4js.setLevel('INFO');
-  return logger4js;
-};
+// exports.logger_log4js = function(name) {
+//   var logger4js = log4js.getLogger(name);
+//   logger4js.setLevel('INFO');
+//   return logger4js;
+// };
 
-app.use(log4js.connectLogger(this.logger_log4js('normal'), {
-    level: 'INFO',
-}));
+// app.use(log4js.connectLogger(this.logger_log4js('normal'), {
+//     level: 'INFO',
+// }));
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser('travelpi'));
