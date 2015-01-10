578db20b7fd4da33ee479f2749bd7855b51d5ee7
Demien Li <demien@appannie.com>
2015-01-10 15:03:16 +0800

    format base model

--- pi/model/base_model.js
+++ pi/model/base_model.js
@@ -1,37 +1,36 @@
 var request = require('request'); 
 
+
 var base_model = function(){
-  var self = this;
-  this.originUrl = null;
-  this.urls = null;
+    var self = this;
+    this.originUrl = null;
+    this.urls = null;
 
-  // 获取urls
-  this.getUrls = function() {
-    return this.urls;
-  }
+    // 获取urls
+    this.getUrls = function() {
+        return this.urls;
+    }
  
-  // 配置urls
-  this.setUrls = function(urls) {
-    this.urls = urls;
-    this.originUrl = urls;
-  } 
+    // 配置urls
+    this.setUrls = function(urls) {
+        this.urls = urls;
+        this.originUrl = urls;
+    } 
 
-  this.say = function() {
-    console.log('hi');
-  }
+    this.say = function() {
+        console.log('hi');
+    }
 
   
-  // 请求数据
-  this.get = function(callback) {
-    request(this.getUrls(), function(err, res, data) {
-      if (err) {
-        throw err;
-      }
-      callback(data);
-    });
-  }
+    // 请求数据
+    this.get = function(callback) {
+        request(this.getUrls(), function(err, res, data) {
+            if (err) {throw err;}
+            callback(data);
+        });
+    }
 
-  // 请求数据
+    // 请求数据
     this.cleanGet = function(callback) {
         request(this.getUrls(), function(err, res, data) {
             var error_msg = '';
@@ -55,43 +54,43 @@ var base_model = function(){
         });
     }
 
-  // 获取所有query参数
-  this.parseQuery = function(req) {
-    if (req == null)
-      return {};
-    var querys = req.query;
-    var params = req.params;
-    var form = {};	// e.x. {id:1, name:'10086', type:'mobile'}
-  
-    for (query in querys) {
-      form[query] = querys[query];
-    }
-    for (param in params) {
-      form[param] = params[param];
-    }
-    return form;
-  }
+    // 获取所有query参数
+    this.parseQuery = function(req) {
+        if (req == null)
+            return {};
+        var querys = req.query;
+        var params = req.params;
+        var form = {};	// e.x. {id:1, name:'10086', type:'mobile'}
 
-  // 组建url api
-  this.buildUrl = function(req) {
-    this.urls = this.originUrl;
-    // 获取所有的url参数
-    var form = this.parseQuery(req);
-    var temp = this.urls;
+        for (query in querys) {
+            form[query] = querys[query];
+        }
+        for (param in params) {
+            form[param] = params[param];
+        }
+        return form;
+    }
+
+    // 组建url api
+    this.buildUrl = function(req) {
+        this.urls = this.originUrl;
+        // 获取所有的url参数
+        var form = this.parseQuery(req);
+        var temp = this.urls;
      
-    for (substr in form) {
-      if (temp){
-        temp = temp.replace(substr, form[substr]);
-      }
-    } 
-    this.urls = temp;
-    return this;
-  }  
+        for (substr in form) {
+            if (temp){
+                temp = temp.replace(substr, form[substr]);
+            }
+        } 
+        this.urls = temp;
+        return this;
+    }  
   
-  // 显示url
-  this.dispUrl = function() {
-    console.log(this.urls);
-  }
+    // 显示url
+    this.dispUrl = function() {
+        console.log(this.urls);
+    }
   
 };
 
